48d2acd010a7ff2a14b6e93a3e805571bfb08df7
Priyanshu <04priyanshuuuu@gmail.com>
2026-03-31 19:22:40 +0530

    fixed urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout_view, name='logout'),
-    path('create-admin/', views.create_admin,name='create-admin'),
     path('apis/', views.api_list, name='api_list'),
 ]
