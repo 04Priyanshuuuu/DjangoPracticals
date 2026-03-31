9f39bfd38f4db876ec752c664307f3c2e392e99e
Priyanshu <04priyanshuuuu@gmail.com>
2026-04-01 05:09:45 +0530

    fixed api ui

--- api/views.py
+++ api/views.py
@@ -1,12 +1,25 @@
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import AllowAny
+
 from .models import Book, Movie
 from .serializers import BookSerializer, MovieSerializer
+
+
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+    def enforce_csrf(self, request):
+        return  # <-- disables CSRF
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [CsrfExemptSessionAuthentication, BasicAuthentication]
+    permission_classes = [AllowAny]
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    authentication_classes = [CsrfExemptSessionAuthentication, BasicAuthentication]
+    permission_classes = [AllowAny]
